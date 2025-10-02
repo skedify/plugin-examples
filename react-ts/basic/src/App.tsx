71a6ed1e6caf72e7e189c133a8daf43dda721c1e
Simon VDB <simonvdbroeck@gmail.com>
2025-10-02 16:58:33 +0200

    chore: bump all dependencies

--- react-ts/basic/src/App.tsx
+++ react-ts/basic/src/App.tsx
@@ -1,27 +1,30 @@
 import {
-  type PexipEngagePluginContextProps,
-  PexipEngagePluginProvider,
-  PexipEngagePlugin,
+	PexipEngagePlugin,
+	type PexipEngagePluginContextProps,
+	PexipEngagePluginProvider,
 } from "@pexip-engage-public/plugin-react";
 
 // Replace the placeholder values with your actual enterprise, and remove the `<>` brackets:
-export const PLUGIN_SCRIPT_SRC = "<https://{YOUR-ENTERPRISE-NAME}.plugin.skedify.io/{YOUR-ENTERPRISE-NAME}/pexip-engage-plugin.js>";
+export const PLUGIN_SCRIPT_SRC =
+	"<https://{YOUR-ENTERPRISE-NAME}.plugin.skedify.io/{YOUR-ENTERPRISE-NAME}/pexip-engage-plugin.js>";
 
 function Playground() {
-  return (
-    <div>
-      <PexipEngagePlugin />
-    </div>
-  );
+	return (
+		<div>
+			<PexipEngagePlugin />
+		</div>
+	);
 }
 
-const defaultConfig: PexipEngagePluginContextProps = { scriptSrc: PLUGIN_SCRIPT_SRC };
+const defaultConfig: PexipEngagePluginContextProps = {
+	scriptSrc: PLUGIN_SCRIPT_SRC,
+};
 
 export default function App() {
-  return (
-    <PexipEngagePluginProvider value={defaultConfig}>
-      {/* All <PexipEngagePlugin /> components will now merge their own passed configuration with the defaultConfig value */}
-      <Playground />
-    </PexipEngagePluginProvider>
-  );
+	return (
+		<PexipEngagePluginProvider value={defaultConfig}>
+			{/* All <PexipEngagePlugin /> components will now merge their own passed configuration with the defaultConfig value */}
+			<Playground />
+		</PexipEngagePluginProvider>
+	);
 }
